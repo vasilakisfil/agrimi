21d2d51c976a88d1f740150293f0ebd876d56792
Filippos Vasilakis <vasilakisfil@gmail.com>
2013-07-04 18:48:41 +0300

    Update server to handle multiple requests sequentially, need to update tests though..

--- lib/goatserver.rb
+++ lib/goatserver.rb
@@ -2,6 +2,49 @@ require 'socket'
 require 'parseconfig'
 
 
+class Request
+  attr_reader :type, :url, :flags
+
+  def initialize(request_string)
+    @type, @url, @other = request_string.split " "
+  end
+end
+
+class AnswerWorker
+  attr_reader :request, :response, :serverRoot
+
+  def initialize
+    @serverRoot = "/home/vasilakisfil/Development/goat/spec/serverAssets"
+  end
+
+  def start(client)
+    request = ""
+    while line = client.gets
+      request += line
+      break if line =~ /^\s*$/
+    end
+    @request = Request.new(request)
+
+    # puts request
+
+    @response = "HTTP/1.1 200 OK\r\nDate: Tue, 14 Dec 2010 10:48:45 GMT\r\n"
+    @response += "Server: RubyrnContent-Type: text/html charset=iso-8859-1\r\n\r\n"
+
+    client.puts @response
+    filepath = "#{@serverRoot }#{@request.url}"
+    if File.exists? filepath
+      file = File.open filepath
+      client.puts file.read
+      file.close
+    else
+      client.puts "Could not find file!"
+    end
+    client.close
+  end
+
+end
+
+
 class Goatserver
   attr_reader :configPath, :serverRoot, :port
 
@@ -13,24 +56,17 @@ class Goatserver
   def start
     @server = TCPServer.new(@port)
     client = nil
+    loop {
+      answerWorker = AnswerWorker.new
+      client = @server.accept
 
-    client = @server.accept
-
-    request = ''
-    while line = client.gets
-      request += line
-      break if line =~ /^\s*$/
-    end
-
-    puts request
-    file = File.open "#{@serverRoot }/index.html"
-    client.puts "HTTP/1.1 200 OK\r\nDate: Tue, 14 Dec 2010 10:48:45 GMT\r\n
-                Server: RubyrnContent-Type: text/html;
-                charset=iso-8859-1\r\n\r\n"
-    client.puts file.read
-    file.rewind
-    puts file.read
-    file.close
-    client.close
+      # Initiate new Actor to handle the request
+      answerWorker.start(client)
+    }
+    @server.close
   end
 end
+
+server = Goatserver.new(5060)
+server.start
+
--- spec/goatserver_spec.rb
+++ spec/goatserver_spec.rb
@@ -2,26 +2,45 @@ require 'spec_helper'
 require 'tempfile'
 require 'rest-client'
 
+
+
+describe "AnswerActor" do
+  before do
+
+  end
+
+  context "with valid client" do
+
+
+  end
+
+end
+
+
 describe "Goatserver" do
 
-  before do
+  before(:all) do
+    @serverAssets = "#{Dir.getwd}/spec/serverAssets"
+    @port = 5060
     # configFilePath = "#{Dir.getwd}/spec/serverAssets/goat.conf"
-    @server = Goatserver.new(5050)
+    @server = Goatserver.new(@port)
     Thread.new { @server.start }
   end
 
   context "with full initializer" do
     it "sets the right conf file and port" do
       # expect(@server.serverRoot).to eq("#{Dir.getwd}/spec/serverAssets")
-      expect(@server.port).to eq(5050)
+      expect(@server.port).to eq(@port)
     end
   end
 
   context "Requesting a page by GET" do
     it "shows the html code" do
       htmlFile = "index.html"
-      response = RestClient.get 'http://localhost:5050/index.html'
-      expect(response.to_s).to eq(htmlFile)
+      link = "http://localhost:#{@port}/#{htmlFile}"
+      response = RestClient.get link
+      expect(response.to_str).to eq(File.open("#{@serverAssets}/#{htmlFile}")
+                                        .read)
     end
   end
 
